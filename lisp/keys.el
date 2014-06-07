072ce64301cb1e901c7a2cede16b8ef90391c100
Michael Griffiths <mikey@cich.li>
2014-06-07 18:52:35 +0100

    Tidy keys.el.

--- lisp/keys.el
+++ lisp/keys.el
@@ -1,5 +1,6 @@
 (provide 'keybindings)
 
+;; defuns
 (global-set-key (kbd "C-c f") 'mac-toggle-fullscreen)
 
 (global-set-key (kbd "C-M-'") 'indent-buffer)
@@ -7,18 +8,23 @@
 (global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
 (global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
 
+;; ace-jump-buffer
 (global-set-key (kbd "C-c b") 'ace-jump-buffer)
 (global-set-key (kbd "C-c B") 'ace-jump-buffer-other-window)
 (global-set-key (kbd "C-c C-S-b") 'ace-jump-buffer-in-one-window)
 
+;; ace-jump-mode
 (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
 (global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
 
+;; ace-window
 (global-set-key (kbd "M-o") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 
+;; browse-kill-ring
 (global-set-key (kbd "C-c k") 'browse-kill-ring)
 
+;; CIDER
 (define-key cider-mode-map (kbd "C-c C-b") 'cider-eval-buffer)
 (define-key cider-mode-map (kbd "C-c C-q") 'cider-quit)
 (define-key cider-mode-map (kbd "C-c M-q") 'cider-restart)
@@ -26,44 +32,56 @@
 (define-key cider-repl-mode-map (kbd "C-c C-q") 'cider-quit)
 (define-key cider-repl-mode-map (kbd "C-c M-q") 'cider-restart)
 
+;; company-mode
+(global-set-key (kbd "C-<tab>") 'company-complete)
 (define-key company-mode-map (kbd "C-q") 'company-show-doc-buffer)
 
-(global-set-key (kbd "C-<tab>") 'company-complete)
-
+;; expand-region
 (global-set-key (kbd "C-;") 'er/expand-region)
 
+;; ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (define-key ibuffer-mode-map (kbd "M-o") nil)
 (define-key ibuffer-mode-map (kbd "C-M-o") 'ibuffer-visit-buffer-1-window)
 
+;; imenu
 (global-set-key (kbd "C-.") 'ido-imenu-anywhere)
 
+;; isearch
 (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
 
+;; magit
 (global-set-key (kbd "C-x m") 'magit-status)
 (global-set-key (kbd "C-x l") 'magit-log)
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
 
+;; popwin
 (global-set-key (kbd "C-z") popwin:keymap)
 
+;; smex
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-c M-x") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+;; swoop
 (global-set-key (kbd "C-c C-s") 'swoop)
 (global-set-key (kbd "C-c C-a") 'swoop-multi)
 (global-set-key (kbd "C-c C-M-s") 'swoop-pcre-regexp)
 (global-set-key (kbd "C-c C-S-s") 'swoop-back-to-last-position)
 
+;; webjump
 (global-set-key (kbd "C-x g") 'webjump)
 
+;; whitespace
 (global-set-key (kbd "C-c n") 'whitespace-cleanup)
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 
+;; winner
 (define-key winner-mode-map (kbd "C-c [") 'winner-undo)
 (define-key winner-mode-map (kbd "C-c ]") 'winner-redo)
 
+;; yasnippet
 (define-key yas-minor-mode-map [(tab)] nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 (global-set-key (kbd "<backtab>") 'yas-insert-snippet)
