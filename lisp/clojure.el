a8fb30c3a4e760f97c6305fafc93e29fceaba80c
Michael Griffiths <mikey@cich.li>
2014-09-14 16:55:02 +0100

    Disable clj-refactor-mode and rainbow-delimiters-mode in cider repls

--- lisp/clojure.el
+++ lisp/clojure.el
@@ -16,10 +16,8 @@
   (clj-refactor-mode 1)
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
-(dolist (hook '(cider-repl-mode-hook
-                clojure-mode-hook))
-  (add-hook hook 'enable-clj-refactor-mode)
-  (add-hook hook 'rainbow-delimiters-mode-enable))
+(add-hook 'clojure-mode-hook 'enable-clj-refactor-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode-enable)
 
 (add-hook 'cider-repl-mode-hook 'hide-trailing-whitespace)
 
