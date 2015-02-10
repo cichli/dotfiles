ae127941e92539fca9bfc85ac3589177d99dc39a
Michael Griffiths <mikey@cich.li>
2015-02-10 17:50:31 +0000

    Bump lein-bikeshed dep

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,7 +5,7 @@
                   [com.jakemccrary/lein-test-refresh "0.5.0"]
                   [jonase/eastwood "0.1.4"]
                   [lein-ancient "0.6.2"]
-                  [lein-bikeshed "0.1.7"]
+                  [lein-bikeshed "0.2.0"]
                   [lein-kibit "0.0.8"]
                   [refactor-nrepl "0.2.2"]]
         :injections [(require 'pjstadig.humane-test-output)
