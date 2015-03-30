1228c4c147a501bad5e720800ffdabfdf8bf3528
Michael Griffiths <mikey@cich.li>
2015-03-30 18:47:49 +0100

    Use default CIDER result prefixes

--- .emacs.d/lisp/clojure.el
+++ .emacs.d/lisp/clojure.el
@@ -1,13 +1,11 @@
 (require 'cider)
 
 (setq cider-auto-select-error-buffer t
-      cider-interactive-eval-result-prefix ";; => "
       cider-mode-line nil
       cider-prompt-for-symbol nil
       cider-show-error-buffer 'always
       cider-repl-history-file (concat user-emacs-directory ".cider-history")
       cider-repl-history-size 1000
-      cider-repl-result-prefix ";; => "
       cider-repl-use-clojure-font-lock t
       cider-repl-use-pretty-printing t
       cider-repl-wrap-history t
