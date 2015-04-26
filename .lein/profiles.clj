5920ccff21175e80b735363088575976cb432327
Michael Griffiths <mikey@cich.li>
2015-04-26 01:02:33 +0100

    Bump lein-test-refresh dep

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,7 @@
         :dependencies [[criterium "0.4.3"]
                        [pjstadig/humane-test-output "0.7.0"]]
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [com.jakemccrary/lein-test-refresh "0.7.0"]
+                  [com.jakemccrary/lein-test-refresh "0.9.0"]
                   [jonase/eastwood "0.2.1"]
                   [lein-ancient "0.6.7"]
                   [lein-bikeshed "0.2.0"]
@@ -10,4 +10,5 @@
                   [lein-kibit "0.0.8"]
                   [refactor-nrepl "1.0.1"]]
         :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+                     (pjstadig.humane-test-output/activate!)]
+        :test-refresh {:quiet true}}}
