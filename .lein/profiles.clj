10284cb54b4d1341c0db969168080bfee12f06b1
Michael Griffiths <mikey@cich.li>
2015-07-05 14:43:35 +0100

    cider-nrepl -> 0.10.0-SNAPSHOT

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:jvm-opts ["-Dapple.awt.UIElement=true" "-XX:-OmitStackTraceInFastThrow"]
         :dependencies [[criterium "0.4.3"]
                        [pjstadig/humane-test-output "0.7.0"]]
-        :plugins [[cider/cider-nrepl "0.9.1-SNAPSHOT"]
+        :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.10.0"]
                   [jonase/eastwood "0.2.1"]
                   [lein-ancient "0.6.7"]
