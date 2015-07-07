dded152546ca1dff2e2443c8b1dbd1bf0b06fa00
Michael Griffiths <mikey@cich.li>
2015-07-07 10:49:44 +0100

    lein-cljfmt -> 0.2.0

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,7 +7,7 @@
                   [jonase/eastwood "0.2.1"]
                   [lein-ancient "0.6.7"]
                   [lein-bikeshed "0.2.0"]
-                  [lein-cljfmt "0.1.10"]
+                  [lein-cljfmt "0.2.0"]
                   [lein-kibit "0.1.2"]
                   [refactor-nrepl "1.1.0-SNAPSHOT"]
                   [venantius/yagni "0.1.1"]]
