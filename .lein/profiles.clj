f581dcf1e7c4eb169b2f76e6d167df5f33b0b1df
Michael Griffiths <mikey@cich.li>
2015-08-06 15:55:37 +0100

    Bump cljfmt -> 0.3.0

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,7 +7,7 @@
                   [jonase/eastwood "0.2.1"]
                   [lein-ancient "0.6.7"]
                   [lein-bikeshed "0.2.0"]
-                  [lein-cljfmt "0.2.0"]
+                  [lein-cljfmt "0.3.0"]
                   [lein-kibit "0.1.2"]
                   [lein-pdo "0.1.1"]
                   [refactor-nrepl "1.2.0-SNAPSHOT"]
