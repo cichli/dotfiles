9f3b46941648ea4f8ae4f316bf348ea936aaa369
Michael Griffiths <mikey@cich.li>
2015-08-12 08:45:46 +0100

    Remove explicit tools.nrepl dep

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user {:jvm-opts ["-Dapple.awt.UIElement=true" "-XX:-OmitStackTraceInFastThrow"]
         :dependencies [[criterium "0.4.3"]
-                       [pjstadig/humane-test-output "0.7.0"]
-                       [org.clojure/tools.nrepl "0.2.10"]]
+                       [pjstadig/humane-test-output "0.7.0"]]
         :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.10.0"]
                   [jonase/eastwood "0.2.1"]
