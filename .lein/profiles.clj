b3e51ddd670111605d35afca5d2f07efa6b19191
Michael Griffiths <mikey@cich.li>
2015-10-31 14:49:29 +0000

    Code style

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,9 @@
 {:user {:jvm-opts ["-Dapple.awt.UIElement=true" "-XX:-OmitStackTraceInFastThrow"]
+
         :dependencies [[criterium "0.4.3"]
                        [org.clojure/tools.nrepl "0.2.11"]
                        [pjstadig/humane-test-output "0.7.0"]]
+
         :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.11.0"]
                   [jonase/eastwood "0.2.1"]
@@ -12,8 +14,10 @@
                   [lein-pdo "0.1.1"]
                   [lein-pprint "1.1.2"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
+
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
+
         :test-refresh {:quiet true}}
 
  :flight-recorder {:jvm-opts ["-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder"]}}
