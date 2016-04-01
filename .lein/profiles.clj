7cc403f06a73941f8b3ef08d109ca87c89907092
Michael Griffiths <mikey@cich.li>
2016-04-01 12:39:34 +0100

    lein-bikeshed -> 0.3.0

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -9,7 +9,7 @@
                   [com.jakemccrary/lein-test-refresh "0.14.0"]
                   [jonase/eastwood "0.2.3"]
                   [lein-ancient "0.6.9"]
-                  [lein-bikeshed "0.2.0"]
+                  [lein-bikeshed "0.3.0"]
                   [lein-cljfmt "0.3.0"]
                   [lein-kibit "0.1.2"]
                   [lein-pdo "0.1.1"]
