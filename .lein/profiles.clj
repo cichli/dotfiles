3ad6bd6173a1dcdea6198d9d7458c03ffc5f3ca0
Michael Griffiths <mikey@cich.li>
2016-12-08 16:31:57 +0000

    lein-kibit 0.1.3

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -11,7 +11,7 @@
                   [lein-ancient "0.6.10"]
                   [lein-bikeshed "0.4.1"]
                   [lein-cljfmt "0.5.6"]
-                  [lein-kibit "0.1.2"]
+                  [lein-kibit "0.1.3"]
                   [lein-pprint "1.1.2"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]
 
