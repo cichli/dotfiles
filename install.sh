7c95e1298c7415ea2bfdf16e3f5cf0112f417536
Michael Griffiths <mikey@cich.li>
2017-04-14 23:36:27 +0100

    Lesson learned :(

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+pushd ~/.dotfiles
+
 for file in $(find `pwd` -mindepth 1 -maxdepth 1 ! \( -name '.git' -or -name '.gitignore' -or -name 'install.sh' \)); do
     ln -Ffhs $file ~/$(basename $file)
 done
+
+popd
