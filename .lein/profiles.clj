c1404cecd6bf23c225e6b3115ded03e45b5e2a6a
Michael Griffiths <mikey@cich.li>
2017-09-18 11:20:46 +0100

    eastwood -> 0.2.4

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,7 +7,7 @@
 
         :plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.21.1"]
-                  [jonase/eastwood "0.2.3"]
+                  [jonase/eastwood "0.2.4"]
                   [lein-ancient "0.6.10"]
                   [lein-bikeshed "0.4.1"]
                   [lein-cljfmt "0.5.6"]
