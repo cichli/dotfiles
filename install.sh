4776043546cdb90d155e7124ca9d57f3eea5a691
Michael Griffiths <mikey@cich.li>
2019-02-21 21:00:43 +0000

    Use /usr/bin/env bash

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 pushd ~/.dotfiles
 
-for file in $(find `pwd` -mindepth 1 -maxdepth 1 ! \( -name '.git' -or -name '.gitignore' -or -name 'install.sh' \)); do
+for file in $(find `pwd` -mindepth 1 -maxdepth 1 ! \( -name '.git' -or -name '.gitignore' -or -name 'install.sh' -or -name 'update.sh' \)); do
     ln -Ffhs $file ~/$(basename $file)
 done
 
