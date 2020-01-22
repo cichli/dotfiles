af41505d75a4210232d546422f7d8224acd52000
Michael Griffiths <mikey@cich.li>
2020-01-22 16:33:48 +0000

    Ensure terminal title is correct after quitting SSH

--- .config/liquidprompt.ps1
+++ .config/liquidprompt.ps1
@@ -1,1 +1,1 @@
-export LP_PS1=`echo "\${LP_TIME}[\${LP_USER}\${LP_HOST}\${LP_PERM}\${LP_PWD}]\${LP_VCS}\n\${LP_MARK}"`
+export LP_PS1=`echo "\[\033]0;\007\]\${LP_TIME}[\${LP_USER}\${LP_HOST}\${LP_PERM}\${LP_PWD}]\${LP_VCS}\n\${LP_MARK}"`
