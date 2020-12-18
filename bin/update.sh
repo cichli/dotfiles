542e030bb0f787bf790d108461fd2aa4932bb825
Michael Griffiths <mikey@cich.li>
2020-12-18 12:55:50 +0000

    Don't cleanup automatically

--- bin/update.sh
+++ bin/update.sh
@@ -5,7 +5,6 @@ set -e
 
 brew update
 brew bundle dump --global --force
-brew bundle cleanup --global --force
 brew bundle install --global --verbose --no-upgrade
 brew upgrade --fetch-HEAD
 mas upgrade
